3ac2717429065c6fcf7b1a5222254d8103162069
Education-cite <105702139+Education-cite@users.noreply.github.com>
2025-04-11 12:32:52 +0600

    questionScreen
    Conditional Rendering & Lifting State Up

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:quiz_app/questions_screen.dart';
 import 'package:quiz_app/start_screen.dart';
 
 class Quiz extends StatefulWidget {
@@ -9,7 +10,19 @@ class Quiz extends StatefulWidget {
 }
 
 class _QuizState extends State<Quiz> {
-  var activeScreen = StartScreen();
+  Widget? activeScreen;
+  @override
+  void initState() {
+    super.initState();
+    activeScreen = StartScreen(swichScreen);
+  }
+
+  void swichScreen() {
+    setState(() {
+      activeScreen = QuestionsScreen();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
